390dd061b2a230fd423dc04ce41085a5ab263afe
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-06-25 14:55:18 +0530

    .

--- portfolio-swapnil/src/Components/Contact.jsx
+++ portfolio-swapnil/src/Components/Contact.jsx
@@ -4,32 +4,33 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import EmailIcon from "@mui/icons-material/Email";
 import CallIcon from "@mui/icons-material/Call";
-import Grid from "@mui/material/Grid";
 const Contact = () => {
   return (
     <div id="contact">
       <h1>Get in touch</h1>
-      <div className="contacts">
-        <Grid
-          container
-          direction="row"
-          alignItems="center"
-          className="icons-grid"
-        >
-          <EmailIcon className="icons" />
-          &nbsp; swapnilm1908@gmail.com
-        </Grid>
+      <a
+        className="icons-container"
+        href="https://mail.google.com/mail/?view=cm&to=swapnilm1908@gmail.com"
+        target="_blank"
+      >
+        <EmailIcon className="icon" />
+        <div className="icon-text"> Swapnilm1908@gmail.com</div>
+      </a>
+      <div className="icons-container">
+        <CallIcon className="icon" />
+        <div className="icon-text">9595183013</div>
       </div>
-      <div>
-        <Grid
-          container
-          direction="row"
-          alignItems="center"
-          className="icons-grid"
-        >
-          <Contact className="icons" />
-          &nbsp; 9595183013
-        </Grid>
+
+      <div className="icon-container">
+        <a href="https://github.com/SwapnilMeshram19" target="_blank">
+          <GitHubIcon className="icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/swapnil-meshram-37a715167" target="_blank">
+          <LinkedInIcon className="icon" />
+        </a>
+        <a href="https://twitter.com/swapnilm1908" target="_blank">
+          <TwitterIcon className="icon"  />
+        </a>
       </div>
     </div>
   );
--- portfolio-swapnil/src/Components/Navbar.jsx
+++ portfolio-swapnil/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Navbar.css";
 import { Link } from "react-scroll";
-import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+
 
 const Navbar = () => {
   return (
