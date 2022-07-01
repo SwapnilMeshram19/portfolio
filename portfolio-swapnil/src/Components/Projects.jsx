099b979023fa3e89d3c502af597c3aba0b89e1d5
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-07-01 15:01:31 +0530

    Update Projects.jsx

--- portfolio-swapnil/src/Components/Projects.jsx
+++ portfolio-swapnil/src/Components/Projects.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactPlayer from "react-player"
-import mg from "./videos/1mg.mp4"
+import mg from "./videos/1mg.mp4";
+import bookmyshow from "./videos/bookmyshow.mp4";
+import dineout from "./videos/dineout.mp4"
+
 const Projects = () => {
+
+  
   return (
     <div id="projects">
       <h1>Projects</h1>
@@ -14,6 +19,29 @@ const Projects = () => {
            playing={true}
            muted={true}
            playbackRate={0.5}
+           controls={false}
+           config={{ file: { 
+            attributes: {
+              controlsList: 'nodownload'  
+            }
+        }}}
+           onContextMenu={e => e.preventDefault()}
+           onEnded={()=>this.onEnded()}
+          />
+          <ReactPlayer
+           url={bookmyshow}
+           muted={true}
+           playbackRate={0.5}
+           loop={true}
+           playing={true}
+           controls={false}
+           config={{ file: { 
+            attributes: {
+              controlsList: 'nodownload' 
+            }
+        }}}
+        onContextMenu={e => e.preventDefault()}
+           onEnded={()=>this.onEnded()}
           />
         </div>
 
